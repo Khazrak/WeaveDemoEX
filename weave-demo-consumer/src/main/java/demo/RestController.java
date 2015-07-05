3d4ee0e8e812fb2a3a7e237ab1f45e45e867db93
Karl Andersson <karl.andersson1987@gmail.com>
2015-07-05 20:43:34 +0200

    Trying with Retry strategy

--- weave-demo-consumer/src/main/java/demo/RestController.java
+++ weave-demo-consumer/src/main/java/demo/RestController.java
@@ -1,5 +1,7 @@
 package demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
@@ -17,14 +19,47 @@ import java.util.ResourceBundle;
 @RequestMapping("/rest")
 public class RestController {
 
+    private static final int RETRIES = 10;
+
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     public String getFromService() {
+
+        int tries = 0;
+
         ResourceBundle properties = ResourceBundle.getBundle("service");
-        RestTemplate restTemplate = new RestTemplate();
 
         String serviceHostname = properties.getString("servicename");
 
-        String res = restTemplate.getForObject("http://" + serviceHostname + ":8080/rest/hostname", String.class);
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        String res = null;
+
+        boolean loop = true;
+
+        while(loop) {
+
+            ResponseEntity<String> response = restTemplate.getForEntity(
+                    "http://" + serviceHostname + ":8080/rest/hostname",
+                    String.class);
+
+            if (HttpStatus.OK == response.getStatusCode()) {
+                res = response.getBody();
+                loop = false;
+            } else {
+                sleep(10);
+                tries++;
+                if(tries >= 10)
+                {
+                    loop = true;
+                    res = "Service Unavailable, try again later";
+                }
+            }
+        }
+
+
+
+        //String res = restTemplate.getForObject("http://" + serviceHostname + ":8080/rest/hostname", String.class);
 
         String sysEnvStr = System.getenv("SERVICE_NAME");
 
@@ -36,5 +71,13 @@ public class RestController {
         return "Consumer: " + sysEnvStr + ", Producer: " + res + "\n";
     }
 
+    private void sleep(int seconds)
+    {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
